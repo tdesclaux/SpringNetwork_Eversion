d0c14380707acc1725bf9ba8054830a75fc53fc9
tdesclaux <tdesclaux@gmail.com>
2026-03-04 14:25:09 +0100

    avec la légende

--- bending/analyse_torsion.py
+++ bending/analyse_torsion.py
@@ -2,22 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 from collections import defaultdict
-import matplotlib as mpl
-mpl.rcParams["xtick.direction"] = "in"
-mpl.rcParams["ytick.direction"] = "in"
-mpl.rcParams["lines.markeredgecolor"] = "k"
-mpl.rcParams["lines.markeredgewidth"] = 1
-mpl.rcParams["figure.dpi"] = 250
-mpl.rcParams['legend.frameon']= False
-mpl.rcParams['legend.fontsize']= 20
-mpl.rcParams['axes.labelsize'] = 20
-from matplotlib import rc
-rc('font', family='serif')
-rc('text', usetex=True)  # Correction ici
-rc('xtick', labelsize=20)
-rc('ytick', labelsize=20)
-Color_python = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-# Parallel
+
 NX, NY, NZ = 10, 5, 1
 
 
@@ -90,10 +75,14 @@ for B in [0.05,0.1,0.5]:
             delta.append(z[IDX_delta])
         
         # plt.plot(Y[IDX_PLOT], Z[IDX_PLOT], '--', label='initial')
-        plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib],label=f'déformé incrément={step}')
+        if step == 15:
+            plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib],label=f'B={B}')
+        else:
+            plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib])
         # plt.axis('equal')
         plt.xlabel('x (m)'); plt.ylabel('$u_z B/P$ (m$^5$)')
         plt.grid(True, alpha=0.3)
     ib = ib+1
-
+    
+plt.legend()
 plt.show()
