f9d9f0a09aca309ac2f1135ff4f3d8a5ef62eeb3
tdesclaux <tdesclaux@gmail.com>
2026-03-04 14:29:48 +0100

    Update analyse_torsion.py

--- bending/analyse_torsion.py
+++ bending/analyse_torsion.py
@@ -80,9 +80,73 @@ for B in [0.05,0.1,0.5]:
         else:
             plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib])
         # plt.axis('equal')
-        plt.xlabel('x (m)'); plt.ylabel('$u_z B/P$ (m$^5$)')
+        plt.xlabel('x (m)',fontsize=20); plt.ylabel('$u_z B/P$ (m$^5$)',fontsize=20)
         plt.grid(True, alpha=0.3)
     ib = ib+1
     
-plt.legend()
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.legend(fontsize=20)
+plt.show()
+
+
+
+path = r"./data_torquespring"
+ib = 0
+plt.figure()
+colB = [[0,0.8,0],[1,0.5,0],[1,0,0]]
+for B in [0.05,0.1,0.5]:
+    filepath_r = path + r"/data_NX=10_NY=5_NZ=1_B="+str(B)+".csv"
+    filepath_F = path + r"/P_save_NX=10_NY=5_NZ=1_B="+str(B)+".csv"
+    # dZ=0.5
+    # filepath_r = path + r"/data_NX=10_NY=5_NZ=3_dZ="+str(dZ)+".csv"
+    # filepath_F = path + r"/P_save_NX=10_NY=5_NZ=3_dZ="+str(dZ)+".csv"
+    
+    # Lit la force
+    P_save = [0.]
+    with open(filepath_F, "r") as f:
+        reader = csv.reader(f)
+        next(reader)  # skip header
+    
+        for row in reader:
+            F = float(row[0])
+            P_save.append(F)
+    
+    r_save,n_step = produce_r_save(filepath_r)
+    
+    # PLOTS :
+    delta=[0.]
+    NMAX=99
+    for step in [NMAX]:
+        r = r_save[step]
+        r0 = r_save[0]
+        
+        x = r[:, 0]; y = r[:, 1]; z = r[:, 2]
+        X = r0[:, 0]; Y = r0[:, 1]; Z = r0[:, 2]
+        
+        
+        
+    
+        slice_y = 2
+        IDX_delta = node_index(NX-1,slice_y,0)
+        IDX_PLOT = np.array([node_index(ix, slice_y, 0) for ix in range(NX)])
+        IDX_PLOT2 = np.array([node_index(ix, slice_y, 1) for ix in range(NX)])
+        IDX_PLOT3 = np.array([node_index(ix, slice_y, 2) for ix in range(NX)])
+        if (step>0):
+            delta.append(z[IDX_delta])
+        
+        # plt.plot(Y[IDX_PLOT], Z[IDX_PLOT], '--', label='initial')
+        if step == NMAX:
+            plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib],label=f'B={B}')
+        else:
+            plt.plot(x[IDX_PLOT], z[IDX_PLOT]/P_save[step]*B, '-',color=colB[ib])
+        # plt.axis('equal')
+        plt.xlabel('x (m)',fontsize=20); plt.ylabel('$u_z B/P$ (m$^5$)',fontsize=20)
+        plt.grid(True, alpha=0.3)
+    ib = ib+1
+    
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.xlim(-2,9)
+plt.legend(fontsize=20)
 plt.show()
